0ce066caa417ea6036134b1aa45b40438af165ea
RahimliZakir <zakirer@code.edu.az>
2022-01-12 19:41:24 +0400

    jQuery Validate On Client Side Done

---
+++ RyanP410.WebUI/AppCode/Modules/ContactsModule/ContactSendMessageCommand.cs
@@ -0,0 +1,87 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using RyanP410.WebUI.AppCode.Extensions;
+using RyanP410.WebUI.AppCode.Infrastructure;
+using RyanP410.WebUI.Models.DataContexts;
+using RyanP410.WebUI.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace RyanP410.WebUI.AppCode.Modules.ContactsModule
+{
+    public class ContactSendMessageCommand : IRequest<JsonCommandResponse>
+    {
+        [Required(ErrorMessage = "Bu hissə boş qoyula bilməz!")]
+        public string FullName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Bu hissə boş qoyula bilməz!")]
+        [EmailAddress(ErrorMessage = "Xahiş olunur Email formatında daxil edin!")]
+        public string EmailAddress { get; set; } = null!;
+
+        [Required(ErrorMessage = "Bu hissə boş qoyula bilməz!")]
+        public string Message { get; set; } = null!;
+
+        public class ContactSendMessageCommandHandler : IRequestHandler<ContactSendMessageCommand, JsonCommandResponse>
+        {
+            readonly RyanDbContext db;
+            readonly IActionContextAccessor ctx;
+
+            public ContactSendMessageCommandHandler(RyanDbContext db, IActionContextAccessor ctx)
+            {
+                this.db = db;
+                this.ctx = ctx;
+            }
+
+            async public Task<JsonCommandResponse> Handle(ContactSendMessageCommand request, CancellationToken cancellationToken)
+            {
+                JsonCommandResponse response = new();
+
+                if (!ctx.IsValid())
+                {
+                    response.Error = true;
+                    response.Message = "Məlumat tamlığı qorunmayıb!";
+
+                    goto end;
+                }
+
+                if (!request.EmailAddress.IsEmail())
+                {
+                    response.Error = true;
+                    response.Message = "Xahiş olunur Email formatında daxil edin!";
+
+                    goto end;
+                }
+
+                if (ctx.IsValid())
+                {
+                    try
+                    {
+                        Contact contact = new()
+                        {
+                            FullName = request.FullName,
+                            EmailAddress = request.EmailAddress,
+                            Message = request.Message
+                        };
+
+                        await db.Contacts.AddAsync(contact, cancellationToken);
+                        await db.SaveChangesAsync(cancellationToken);
+
+                        response.Error = false;
+                        response.Message = "Müraciətiniz uğurla bizə göndərildi!";
+
+                        goto end;
+                    }
+                    catch (Exception)
+                    {
+                        response.Error = true;
+                        response.Message = "Bilinməyən bir xəta baş verdi, bir neçə dəqiqə sonra yenidən yoxlayın!";
+
+                        goto end;
+                    }
+                }
+
+            end:
+                return response;
+            }
+        }
+    }
+}
--- RyanP410.WebUI/Controllers/HomeController.cs
+++ RyanP410.WebUI/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RyanP410.WebUI.AppCode.Extensions;
+using RyanP410.WebUI.AppCode.Infrastructure;
+using RyanP410.WebUI.AppCode.Modules.ContactsModule;
 using RyanP410.WebUI.Models.DataContexts;
 using RyanP410.WebUI.Models.Entities;
 
@@ -9,11 +12,11 @@ namespace RyanP410.WebUI.Controllers
 {
     public class HomeController : Controller
     {
-        readonly RyanDbContext db;
+        readonly IMediator mediator;
 
-        public HomeController(RyanDbContext db)
+        public HomeController(IMediator mediator)
         {
-            this.db = db;
+            this.mediator = mediator;
         }
 
         [AllowAnonymous]
@@ -31,47 +34,11 @@ namespace RyanP410.WebUI.Controllers
 
         [HttpPost]
         [AllowAnonymous]
-        public async Task<IActionResult> SendContactMessage([Bind("FullName", "EmailAddress", "Message")] Contact contact)
+        public async Task<IActionResult> SendContactMessage([Bind("FullName", "EmailAddress", "Message")] ContactSendMessageCommand request)
         {
+            JsonCommandResponse response = await mediator.Send(request);
 
-            if (!ModelState.IsValid)
-                return Json(new
-                {
-                    error = true,
-                    message = "Məlumat tamlığı qorunmayıb!"
-                });
-
-            if (!contact.EmailAddress.IsEmail())
-                return Json(new
-                {
-                    error = true,
-                    message = "Xahiş olunur Email formatında daxil edin!"
-                });
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    await db.Contacts.AddAsync(contact);
-                    await db.SaveChangesAsync();
-
-                    return Json(new
-                    {
-                        error = false,
-                        message = "Müraciətiniz uğurla bizə göndərildi!"
-                    });
-                }
-                catch (Exception)
-                {
-                    return Json(new
-                    {
-                        error = true,
-                        message = "Bilinməyən bir xəta baş verdi, bir neçə dəqiqə sonra yenidən yoxlayın!"
-                    });
-                }
-            }
-
-            return View();
+            return Json(response);
         }
     }
 }
