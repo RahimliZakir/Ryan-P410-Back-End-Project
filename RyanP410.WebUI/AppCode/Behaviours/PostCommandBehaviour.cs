2c271e2a65c6b60fdc30481a087e03f27e5b2fd2
RahimliZakir <zakirer@code.edu.az>
2022-01-19 15:28:09 +0400

    MediatR PostCommandRequest Fixed

--- RyanP410.WebUI/AppCode/Behaviours/PostCommandBehaviour.cs
+++ RyanP410.WebUI/AppCode/Behaviours/PostCommandBehaviour.cs
@@ -1,23 +1,25 @@
-﻿using MediatR.Pipeline;
+﻿using MediatR;
+using MediatR.Pipeline;
 using RyanP410.WebUI.Models.DataContexts;
 
 namespace RyanP410.WebUI.AppCode.Behaviours
 {
-    //public class PostCommandBehaviour<TRequest, TResponse> : IRequestPostProcessor<TRequest, TResponse>
-    //{
-    //    readonly RyanDbContext db;
+    public class PostCommandBehaviour<TRequest, TResponse> : IRequestPostProcessor<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        readonly RyanDbContext db;
 
-    //    public PostCommandBehaviour(RyanDbContext db)
-    //    {
-    //        this.db = db;
-    //    }
+        public PostCommandBehaviour(RyanDbContext db)
+        {
+            this.db = db;
+        }
 
-    //    public async Task Process(TRequest request, TResponse response, CancellationToken cancellationToken)
-    //    {
-    //        if (db.Database.CurrentTransaction != null)
-    //        {
-    //            await db.Database.CurrentTransaction.CommitAsync(cancellationToken);
-    //        }
-    //    }
-    //}
+        async public Task Process(TRequest request, TResponse response, CancellationToken cancellationToken)
+        {
+            if (db.Database.CurrentTransaction != null)
+            {
+                await db.Database.CurrentTransaction.CommitAsync(cancellationToken);
+            }
+        }
+    }
 }
