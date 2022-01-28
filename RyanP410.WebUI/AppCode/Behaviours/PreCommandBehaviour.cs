faa5b0a7de4e037702f9dfd1d62824d782719e64
RahimliZakir <zakirer@code.edu.az>
2022-01-29 00:03:47 +0400

    PreCommandBehaviour Improved

--- RyanP410.WebUI/AppCode/Behaviours/PreCommandBehaviour.cs
+++ RyanP410.WebUI/AppCode/Behaviours/PreCommandBehaviour.cs
@@ -1,21 +1,22 @@
 ﻿using MediatR.Pipeline;
-using Microsoft.EntityFrameworkCore.Storage;
 using RyanP410.WebUI.Models.DataContexts;
 
 namespace RyanP410.WebUI.AppCode.Behaviours
 {
-    //public class PreCommandBehaviour<T> : IRequestPreProcessor<T>
-    //{
-    //    readonly RyanDbContext db;
+    public class PreCommandBehaviour<T> : IRequestPreProcessor<T>
+        where T : notnull
+    {
+        readonly RyanDbContext db;
 
-    //    public PreCommandBehaviour(RyanDbContext db)
-    //    {
-    //        this.db = db;
-    //    }
+        public PreCommandBehaviour(RyanDbContext db)
+        {
+            this.db = db;
+        }
 
-    //    public async Task Process(T request, CancellationToken cancellationToken)
-    //    {
-    //        IDbContextTransaction transaction = await db.Database.BeginTransactionAsync(cancellationToken);
-    //    }
-    //}
+        public async Task Process(T request, CancellationToken cancellationToken)
+        {
+            if (db.Database.CurrentTransaction != null)
+                await db.Database.BeginTransactionAsync(cancellationToken);
+        }
+    }
 }
